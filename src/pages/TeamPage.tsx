17b79d06f806d9fbbfff0cc8c9d45d5228dac1ff
decrefy <erwin.rdf@gmail.com>
2022-01-06 16:06:43 +0800

    adding drawer component in TeamPage

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,6 +1,15 @@
 import {
+  Box,
   Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
   Flex,
+  FormLabel,
   HStack,
   Input,
   InputGroup,
@@ -10,10 +19,18 @@ import {
   useDisclosure,
   useMediaQuery,
 } from "@chakra-ui/react";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 import { FaSearch } from "react-icons/fa";
+import { toast } from "react-toastify";
 import HeadingComponent from "../components/Heading";
 import StyleContext from "../context/StyleContext";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required("Team name is required").min(5),
+});
 
 const SubHeadingComponent = ({
   setSearch,
@@ -74,6 +91,83 @@ const SubHeadingComponent = ({
   );
 };
 
+const DrawerComponent = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  const firstField = useRef(null);
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors, isDirty, isValid },
+  } = useForm<{ name: string }>({
+    mode: "onChange",
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data: { name: string }) => {
+    try {
+      console.log(data);
+    } catch (err: any) {
+      toast.error(err.data.message);
+    }
+  };
+
+  return (
+    <Drawer
+      isOpen={isOpen}
+      placement="right"
+      initialFocusRef={firstField}
+      onClose={onClose}
+      closeOnOverlayClick={false}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">Create a new team</DrawerHeader>
+
+          <DrawerBody>
+            <Stack spacing="24px">
+              <Box>
+                <FormLabel htmlFor="email">Team Name</FormLabel>
+                <Input
+                  autoComplete="off"
+                  id="name"
+                  placeholder="Please enter name"
+                  {...register("name")}
+                />
+                <Text textAlign="left" fontSize="xs" p={1} color="danger">
+                  {errors.name?.message}
+                </Text>
+              </Box>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter borderTopWidth="1px">
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button
+              disabled={!isDirty || !isValid}
+              colorScheme="blue"
+              type="submit"
+              // isLoading={isLoading}
+            >
+              Submit
+            </Button>
+          </DrawerFooter>
+        </form>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
 const TeamPage = () => {
   const {
     isOpen: isDrawerOpen,
@@ -103,6 +197,10 @@ const TeamPage = () => {
           setSearch={setSearch}
         />
       </Flex>
+
+      {isDrawerOpen && (
+        <DrawerComponent isOpen={isDrawerOpen} onClose={closeDrawer} />
+      )}
     </React.Fragment>
   );
 };
